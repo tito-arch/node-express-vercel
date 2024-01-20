dc246c0f85e28941cdebfbe2001b81c59871c2cc
tito-arch <therabbitplc@gmail.com>
2024-01-20 11:26:29 +0300

    finalize local development tests

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-// Import packages
+require("dotenv").config();
 const express = require("express");
-const home = require("./routes/home");
-const leads = require("./routes/leads")
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
-// Middlewares
 const app = express();
-app.use(express.json());
+const port = 5001;
+const cors = require("cors");
 
-// Routes
-app.use("/home", home);
-app.use("/submit-form", leads)
+app.use(bodyParser.json());
+app.use(cors());
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+app.post("/submit-form", async (req, res) => {
+  try {
+    const formData = req.body;
+
+    // Customize the payload
+    const slackPayload = {
+      text: `New form submission:\nEmail: ${formData.email}\nName: ${formData.name}\nPhone: ${formData.phone}\nBudget: ${formData.budget}\nProject Description: ${formData.projectDescription}`,
+    };
+
+    // Slack
+    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+
+    // Send the payload to Slack
+    await axios.post(slackWebhookUrl, slackPayload);
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error handling form submission:", error);
+    res.status(500).json({
+      success: false,
+      error: "Something went wrong. Please try again later.",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+  try {
+    // Your Slack webhook URL
+    const slackWebhookUrl =
+      "https://hooks.slack.com/services/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";
+
+    // Message to send to Slack
+    const slackMessage = {
+      text: "The app is working properly!",
+    };
+
+    // Send a POST request to the Slack webhook URL
+    await axios.post(slackWebhookUrl, slackMessage);
+
+    return res.status(200).json({
+      title: "Express Testing",
+      message: "The app is working properly!",
+    });
+  } catch (error) {
+    console.error("Error sending message to Slack:", error.message);
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 });
 
 module.exports = router;
